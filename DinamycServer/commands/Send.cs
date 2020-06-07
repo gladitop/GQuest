f1c51699baa4bd89950f6bc9ff0fe8cf1bb0ffa5
gladitop <dar.almaeva@yandex.ru>
2020-06-07 15:48:00 +0300

    Database\

--- DinamycServer/commands/Send.cs
+++ DinamycServer/commands/Send.cs
@@ -5,25 +5,6 @@ namespace DinamycServer
 {
     public partial class Commands
     {
-        private void SCORE(TcpClient client) //Отправка всех очков клиенту
-        {
-            try
-            {
-                for (long i = 0; i <= 100; i++)
-                {
-                    var info = Database.GetClientInfo(i);
-                    if (info.Point != null)
-                    {
-                        Function.SendClientMessage(client, $"%SCORE:{info.Nick}:{info.Point}");
-                    } 
-                }
-            }
-            catch (Exception e)
-            {
-                Function.WriteColorText($"SCORE:{e.Message}", ConsoleColor.Red);
-
-            }
-        }
         private void STEST(TcpClient client, string[] arguments)
         {
             try
