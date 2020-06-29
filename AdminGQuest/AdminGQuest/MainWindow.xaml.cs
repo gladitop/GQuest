4e35113575f880c53b5f845d529d71352e4f4dab
Gladi <dam.almaev@gmail.com>
2020-06-29 23:02:17 +0300

    Теперь тут нормальный клиент
    Сделал систему настроик и потом буду делать дизайн

---
+++ AdminGQuest/AdminGQuest/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AdminGQuest.Other;
+
+namespace AdminGQuest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Загрузка настройк
+            var SManager = new SettingsManager(Data.PathSave);
+            Data.Settings = SManager.Load();
+
+            InitializeComponent();//Это загрузка формы
+        }
+    }
+}
---
+++ AdminGQuest/AdminGQuest/Other/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminGQuest.Other
+{
+    static public class Data
+    {
+        #region Параметры
+
+        public const string PathSave = "Settings.json";//Вот тут будет сохранятся настройки
+
+        #endregion
+
+        static public SettingsData Settings { get; set; }
+    }
+}
